1f5a7842b744509dd472aa23af03e3dd4acb7c8f
Oracle Public Cloud User <opc@supportagent.subnet06101000.vcn06101000.oraclevcn.com>
2025-07-22 14:39:22 +0000

    Updating with VM code

--- LLMIntegration.py
+++ LLMIntegration.py
@@ -1,4 +1,4 @@
-import os, sys, array, json, re, time, sqlite3, fitz, subprocess
+import os, sys, array, json, re, time, sqlite3, subprocess
 import oracledb, oci
 from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
